00ea585701df3107568f53c7b2142bd8c2a69369
awaわ <a970565@163.com>
2024-06-02 13:09:56 +0800

    可以编译的版本（ 也许是我的链接器没有设置好

--- src/MotooriKosuzu.cpp
+++ src/MotooriKosuzu.cpp
@@ -14,5 +14,5 @@ Kosuzu::Kosuzu() {
 }
 
 Kosuzu::~Kosuzu() {
-
+	
 }
--- src/WritingBrush.h
+++ src/WritingBrush.h
@@ -2,13 +2,13 @@
 * 作为书记更改
 */
 #pragma once
-#include <jsoncpp/json/json.h>
+//#include <jsoncpp/json/json.h>
 #include <iostream>
 
 //配置
 namespace Pen {
 	class ConfigPen {
 	public:
-		static Json::Value ReadFile(std::string Path);
+		//static Json::Value ReadFile(std::string Path);
 	};
 }
