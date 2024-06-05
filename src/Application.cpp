0064d24ba47e874cea597e510860132e1235b7d0
awaわ <a970565@163.com>
2024-06-05 23:46:00 +0800

    test bot

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,5 +1,9 @@
 #include "start.hpp"
 
+void add() {
+	
+}
+
 #include <iostream>
 int main() {
 	MotooriKosuzu::Init("114514");
--- src/MotooriKosuzu.h
+++ src/MotooriKosuzu.h
@@ -3,13 +3,9 @@
 
 #include <iostream>
 #include <memory>
-/*
-自定义
-*/
-
 //最后交给这个类来进行初始化（也就是app本体？）
 namespace MotooriKosuzu {
-
+	//本体daze
 	class Kosuzu {
 	public:
 		static void Init();
