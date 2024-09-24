e42a6f0dec9d868d5b98d8d1473091473466f443
awaわ <a970565@163.com>
2024-09-24 19:57:24 +0800

    Okey

--- src/WritingBrush.cpp
+++ src/WritingBrush.cpp
@@ -462,12 +462,11 @@ void LinuxPen::update(dpp::slashcommand_t* event) {
 
 	if (!32768 == system(R"(cd ./MotooriKosuzu/src;g++ Application.cpp BambooSlips.h Bookshelf.hpp MotooriKosuzu.cpp MotooriKosuzu.h start.hpp WritingBrush.cpp WritingBrush.h -std=c++20 -l"dpp" -l"pthread" -l"jsoncpp" -o Project.out)")) {
 		RobotPen::GetBot()->message_create(dpp::message("プログラムの再コンパイルが完了しました").set_channel_id(event->command.channel_id).set_guild_id(event->command.guild_id));
-		std::cout << system("cp ./MotooriKosuzu/src/Project.out ./") << std::endl;
 	}
 	else
 		RobotPen::GetBot()->message_create(dpp::message("いや、インクがひっくり返った").set_channel_id(event->command.channel_id).set_guild_id(event->command.guild_id));
 
 
-
+	sleep(5);
 	RobotSlips::bot.release();
 }
