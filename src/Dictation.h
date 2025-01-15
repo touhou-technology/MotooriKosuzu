23e4343bab1406da807066f65e97392945e8945b
awaわ <a970565@163.com>
2025-01-16 01:29:15 +0800

    feat(TranslateVoice):function

--- src/Dictation.h
+++ src/Dictation.h
@@ -15,3 +15,17 @@ private:
 	static void Voice();
 	static void AutoComplete();
 };
+
+class TranslateVoice {
+public:
+	struct user_params {
+
+	};
+public:
+	//kay -> user_params
+	void AddUser(dpp::snowflake obj, user_params params);
+	void DelUser(dpp::snowflake obj, user_params params);
+
+private:
+	std::vector<user_params> m_object;
+};
