0e4db421e80ba1a7ab5592853ebfc469d5d6b3b3
awaわ <awalwashig@gmail.com>
2025-05-23 08:16:11 +0800

    format: Stone

--- src/Stone.cpp
+++ src/Stone.cpp
@@ -149,17 +149,11 @@ void StoneTranslationObj::ChangeWrie(nlohmann::json& tmp) {
 
 void StoneTranslationObj::Stone() {
 	RobotSlips::bot->on_message_create([&](const dpp::message_create_t& event) {
-
 		m_instance->create_message({ event });
-
 		});
 
-
-
 	RobotSlips::bot->on_message_update([&](const dpp::message_update_t& event) {
-
 		m_instance->create_message({ event });
-
 		});
 
 	RobotSlips::bot->on_message_delete([&](const dpp::message_delete_t& event) {
