cc069c3b619696c6a109cc45f69c9a1536d2754a
awalwashig <awalwashig@gmail.com>
2026-02-26 05:05:04 +0800

    P2996R13: enum_to_string

--- src/core/core.hpp
+++ src/core/core.hpp
@@ -1,1 +1,19 @@
+#ifndef CORE_HPP
+#define CORE_HPP
 #include "web.h"
+
+#include <meta>
+
+template<typename E, bool Enumerable = std::meta::is_enumerable_type(^^E)>
+  requires std::is_enum_v<E>
+constexpr std::string_view enum_to_string(E value) {
+  if constexpr (Enumerable)
+    template for (constexpr auto e :
+                  std::define_static_array(std::meta::enumerators_of(^^E)))
+      if (value == [:e:])
+        return std::meta::identifier_of(e);
+
+  return "<unnamed>";
+}
+
+#endif /* CORE_HPP */
