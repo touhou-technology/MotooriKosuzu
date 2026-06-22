d12338df363a9b064ae0b862290f5c24959ec9ce
awalwashig <awalwashig@gmail.com>
2026-06-23 04:35:04 +0800

    move scheme dir

--- src/scheme/scheme.cpp
+++ src/scheme/scheme.cpp
@@ -1,1 +1,16 @@
-int main(int argc, char *argv[]) { return 0; }
+extern "C" {
+#include <chibi/eval.h>
+}
+
+int main(int argc, char *argv[]) {
+	sexp ctx = sexp_make_eval_context(nullptr, nullptr, nullptr, 0, 0);
+
+	sexp_load_standard_env(ctx, nullptr, SEXP_SEVEN);
+	sexp_load_standard_ports(ctx, nullptr, stdin, stdout, stderr, 0);
+
+	sexp_eval_string(ctx, "(display \"Hello World\") (newline)", -1, nullptr);
+
+	sexp_destroy_context(ctx);
+
+	return 0;
+}
--- src/test/test.cpp
+++ src/test/test.cpp
@@ -5,21 +5,6 @@
 #include <print>
 #include <ranges>
 
-extern "C" {
-#include <chibi/eval.h>
-}
-
-void TestScheme() {
-	sexp ctx = sexp_make_eval_context(nullptr, nullptr, nullptr, 0, 0);
-
-	sexp_load_standard_env(ctx, nullptr, SEXP_SEVEN);
-	sexp_load_standard_ports(ctx, nullptr, stdin, stdout, stderr, 0);
-
-	sexp_eval_string(ctx, "(display \"Hello World\") (newline)", -1, nullptr);
-
-	sexp_destroy_context(ctx);
-}
-
 #include "core/reflection.hpp"
 
 #define SIMDJSON_STATIC_REFLECTION 1
@@ -121,6 +106,5 @@ int main(int argc, char *argv[]) {
 
 	test();
 
-	TestScheme();
 	return 0;
 }
