ae806f743e2fb7646e3d78fdb01fbff0f9ed3f08
awalwashig <awalwashig@gmail.com>
2026-06-23 03:57:59 +0800

    chibi-scheme

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -5,6 +5,21 @@
 #include <print>
 #include <ranges>
 
+extern "C" {
+#include <chibi/eval.h>
+}
+
+void TestScheme() {
+	sexp ctx = sexp_make_eval_context(nullptr, nullptr, nullptr, 0, 0);
+
+	sexp_load_standard_env(ctx, nullptr, SEXP_SEVEN);
+	sexp_load_standard_ports(ctx, nullptr, stdin, stdout, stderr, 0);
+
+	sexp_eval_string(ctx, "(display \"Hello World\") (newline)", -1, nullptr);
+
+	sexp_destroy_context(ctx);
+}
+
 #include "core/reflection.hpp"
 
 #define SIMDJSON_STATIC_REFLECTION 1
@@ -105,5 +120,7 @@ int main(int argc, char *argv[]) {
 	simdjsontest();
 
 	test();
+
+	TestScheme();
 	return 0;
 }
